f1a1e13d893e6eba7c95f00cc8d0f20568c1384f
Magnetkopf <82427906+Magnetkopf@users.noreply.github.com>
2025-12-14 10:23:38 +0800

    chore: update links

--- src/views/join.vue
+++ src/views/join.vue
@@ -4,23 +4,18 @@
 
 <template>
   <div class="div-central">
-  <h1>Dingtalk</h1>
+
+
+    <h1>Discord</h1>
     <a
-      href="https://qr.dingtalk.com/action/joingroup?code=v1,k1,NeIQCDTT+NTxav9aaYG5qWhj1R1L0e9rvpaKvc4e2vM=&_dt_no_comment=1&origin=11"
+      href="https://discord.gg/XkZw8s599h"
       target="_blank">
       <button class="border large">
-        chit chat
-      </button>
-    </a>
-    <a
-      href="https://qr.dingtalk.com/action/joingroup?code=v1,k1,xpuVGxB2LTnn7MDWINvzMMJMkxDMjLLtrwgzqeBk70M=&_dt_no_comment=1&origin=11"
-      target="_blank">
-      <button class="border large">
-        招新群
+        Join
       </button>
     </a>
 
-    <h1>Discord</h1>
+    <h1>QQ</h1>
     <p>Coming soon</p>
   </div>
 
